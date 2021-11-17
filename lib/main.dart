944a800cf8c267a09a4e9b345fc139645451136b
Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>
2021-11-17 20:34:28 +0530

    tutorial 6 needed to be completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
 
-//tutorial 5 part 1
+//tutorial 6
 void main() {
-  runApp(MyApp());
+  runApp(
+    DevicePreview(
+      builder: (context) => MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My application'),
-        ),
-        body: Center(
-          child: Text(
-            "Hello World",
-            style: TextStyle(fontSize: 20.0, color: Colors.blue),
-          ),
-        ),
+      locale: DevicePreview.of(context).locale, // <--- Add the locale
+      builder: DevicePreview.appBuilder, // <--- Add the builder
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      //home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('My application'),
+//         ),
+//         body: Center(
+//           child: Text(
+//             "Hello World",
+//             style: TextStyle(fontSize: 20.0, color: Colors.blue),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
+
+
+// //tutorial 5 part 1
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text('My application'),
+//         ),
+//         body: Center(
+//           child: Text(
+//             "Hello World",
+//             style: TextStyle(fontSize: 20.0, color: Colors.blue),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 //tutorial 4
 //import 'package:flutter/material.dart';
